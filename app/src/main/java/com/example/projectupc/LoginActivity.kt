af5d3467476568638bd07bca67315393ec937bca
Josue Durand Pebe <josuedurandpebe@MacBook-Pro-de-Josue.local>
2020-09-09 14:27:37 -0500

    first

---
+++ app/src/main/java/com/example/projectupc/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projectupc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/projectupc/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.projectupc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val DURACION_SPLASH = 1000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DURACION_SPLASH.toLong())
+    }
+}
